6ef69529046a0dae4b5fb60a11d45d1945763465
Mark Lastovski <mlastovski@Marks-MacBook-Air.local>
2021-11-19 11:59:25 +0200

    added comments

--- lastovskyi-fix.py
+++ lastovskyi-fix.py
@@ -1,6 +1,9 @@
+# declare variables, fix naming to snake_case
 first_num = 2
 second_num = 3
-sum = first_num + second_num
-print(sum)
 
-# who
+# add variables
+sum = first_num + second_num
+
+# print output to console
+print(sum)
